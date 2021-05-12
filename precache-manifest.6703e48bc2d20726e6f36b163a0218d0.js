2303de13a7ae817d368554532b0de565e49a5e78
kiran <kgwd07@gmail.com>
2021-05-12 20:09:13 +0530

    Updates

---
+++ precache-manifest.6703e48bc2d20726e6f36b163a0218d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8ee2fc0296fd795ee2645d16addd7a",
+    "url": "/ReviewsReact/index.html"
+  },
+  {
+    "revision": "467aa927a0684f93d29e",
+    "url": "/ReviewsReact/static/css/main.ecb6766e.chunk.css"
+  },
+  {
+    "revision": "e1a3c50a15b44825207f",
+    "url": "/ReviewsReact/static/js/2.bdbc782b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReviewsReact/static/js/2.bdbc782b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "467aa927a0684f93d29e",
+    "url": "/ReviewsReact/static/js/main.8ca57ed6.chunk.js"
+  },
+  {
+    "revision": "8e7dfcfaa20d9736c2e9",
+    "url": "/ReviewsReact/static/js/runtime-main.5124b76c.js"
+  }
+]);
